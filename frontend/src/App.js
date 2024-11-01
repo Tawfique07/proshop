ef8bee70994e11d5a7747a06000324f90647b2d7
Toufiqul Islam <mdtoufiq2342@gmail.com>
2024-11-01 20:00:15 +0600

    Frontend Boilerplate Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
-    return <h1>welcome to proshop</h1>;
+    return (
+        <>
+            <Header />
+
+            <main className="py-3">
+                <Container>
+                    <HomeScreen />
+                </Container>
+            </main>
+
+            <Footer />
+        </>
+    );
 };
 
 export default App;
