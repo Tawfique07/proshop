3cea9631a1c01f6207090085401af5330627db8d
Toufiqul Islam <mdtoufiq2342@gmail.com>
2024-11-04 13:33:36 +0600

    React Router Basic Setup Completed
    Basic React Router Setup Completed
    
    Product Details Created with Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
     return (
@@ -11,7 +11,7 @@ const App = () => {
 
             <main className="py-3">
                 <Container>
-                    <HomeScreen />
+                    <Outlet />
                 </Container>
             </main>
 
