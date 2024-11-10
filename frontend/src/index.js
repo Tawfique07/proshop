d32e4937dfebd9cabf9b901a54b9494f752462e6
Toufiqul Islam <mdtoufiq2342@gmail.com>
2024-11-10 11:58:57 +0600

    Shipping Screen Created
    Private route component added
    
    checkout steps component
    
    payment screen created
    
    order controller and routes added
    
    Place order functionality added
    
    Order screen created
    
    User profile screen added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,12 +11,18 @@ import {
 import App from "./App";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
+import PrivateRoute from "./components/PrivateRoute";
 import reportWebVitals from "./reportWebVitals";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
+import OrderScreen from "./screens/OrderScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import ProductScreen from "./screens/ProductScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 import store from "./store";
 
 const router = createBrowserRouter(
@@ -27,6 +33,14 @@ const router = createBrowserRouter(
             <Route path="/cart" element={<CartScreen />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
+
+            <Route path="" element={<PrivateRoute />}>
+                <Route path="/shipping" element={<ShippingScreen />} />
+                <Route path="/payment" element={<PaymentScreen />} />
+                <Route path="/placeorder" element={<PlaceOrderScreen />} />
+                <Route path="/order/:id" element={<OrderScreen />} />
+                <Route path="/profile" element={<ProfileScreen />} />
+            </Route>
         </Route>
     )
 );
